fcfd84da5968bc244bd704115e9953f50e011ea8
manisha28nagarkoti <126867552+manisha28nagarkoti@users.noreply.github.com>
2023-08-02 15:55:20 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -5,11 +5,11 @@ def call(String name){
     object = new  utility()
 
     pipeline{
-        agent any
+        
         stage('print'){
         step{    
            script{
-               sh """ echo hiii """
+              
                object.print(name)
            }
         } 
