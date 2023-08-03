5fbaf4b1db169152bf6534ea59c3bd86c62254b9
manisha28nagarkoti <126867552+manisha28nagarkoti@users.noreply.github.com>
2023-08-03 17:31:54 +0530

    Update utility.groovy

--- src/com/tothenew/utility.groovy
+++ src/com/tothenew/utility.groovy
@@ -1,5 +1,22 @@
 package com.tothenew
 
 def printy(String name){
-    print("hii")
+    pipeline{
+    agent any 
+   stages{
+      stage('print_name'){
+       steps{
+         script{
+            sh ''' echo $name'''
+         }
+       }
+     }
+     stage('print_name1'){
+       steps{
+         script{
+            sh '''echo $name'''
+         }
+       }
+     }
+ }
 }
