baf5901ab03099ae03cfb8f716856c340dab54b0
manisha28nagarkoti <126867552+manisha28nagarkoti@users.noreply.github.com>
2023-08-03 17:30:05 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,24 +1,25 @@
 import com.tothenew.utility
 def call(String name){
  object = new utility()
- pipeline{
-  agent any 
-  stages{
-      stage('print_name'){
-       steps{
-         script{
-            object.printy(name)
-         }
-       }
-     }
-     stage('print_name1'){
-       steps{
-         script{
-            object.printy(name)
-         }
-       }
-     }
- }
+ object.printy(name)
+ // pipeline{
+ //  agent any 
+ //  stages{
+ //      stage('print_name'){
+ //       steps{
+ //         script{
+ //            object.printy(name)
+ //         }
+ //       }
+ //     }
+ //     stage('print_name1'){
+ //       steps{
+ //         script{
+ //            object.printy(name)
+ //         }
+ //       }
+ //     }
+ // }
  
 }
 } 
